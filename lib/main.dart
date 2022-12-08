0701f860345e9966d40dc3b400762bc8e82f6437
PARK <pdm7817@naver.com>
2022-12-09 00:56:26 +0900

    1209 1256

---
+++ lib/main.dart
@@ -0,0 +1,432 @@
+import 'package:flutter/material.dart';
+import 'package:'
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+
+
+
+    theme: ThemeData(primarySwatch : Colors.blue,),
+      home: Scaffold(
+        body: Center(
+          child: MyPage(),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset : false,
+      body : Column(
+          children: <Widget>[
+
+
+
+
+            SizedBox(height: 60,),
+
+            Center(
+              child: Text("마이페이지",style:TextStyle(fontSize: 25,color: Colors.black),),
+            ),
+            SizedBox(height: 90,),
+            Center(
+              child:Container(
+                margin: EdgeInsets.only(top:5,bottom: 0),
+              child: Text("이원찬님, 안녕하세요.",style:TextStyle(fontSize: 15,color: Colors.black),),
+              )
+              ),
+           /* Center(
+                child:Container(
+                  margin: EdgeInsets.only(top:5,bottom: 0),
+                  child: Text("wonchan999@naver.com",style:TextStyle(fontSize: 15,color: Colors.black),),
+                )
+            ),*/
+
+
+            SizedBox(height: 100,),
+            /*
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+            */
+
+
+/*
+            Center(
+                child:Container(
+                  color : Colors.grey,
+                  width : 350,
+                  height : 30,
+                  margin: EdgeInsets.only(top:7,bottom: 0),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text('실시간 주가',style: TextStyle(
+                        color: Colors.white
+                    ),),
+                  ),
+                )
+            ),
+
+
+            Center(
+                child:Container(
+                  color : Colors.grey,
+                  width : 350,
+                  height : 30,
+                  margin: EdgeInsets.only(top:7,bottom: 100),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text('수익률 확인',style: TextStyle(
+                        color: Colors.white
+                    ),),
+                  ),
+                )
+            ),
+*/
+
+            ElevatedButton(
+              onPressed: (){
+                print('내 정보 수정');
+              },
+              child: Text('내 정보 수정'),
+
+
+              //ElevatedButton 은 backgroundColor 속성이 없다.
+              //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey,
+
+                minimumSize: Size(350,30),
+
+              ),
+            ),
+
+            ElevatedButton(
+              onPressed: (){
+                print('실시간 주가');
+              },
+              child: Text('실시간 주가'),
+
+
+              //ElevatedButton 은 backgroundColor 속성이 없다.
+              //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey,
+
+                minimumSize: Size(350,30),
+
+              ),
+            ),
+
+
+
+            ElevatedButton(
+              onPressed: (){
+                print('수익률 확인');
+              },
+              child: Text('수익률 확인'),
+
+
+              //ElevatedButton 은 backgroundColor 속성이 없다.
+              //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey,
+
+                minimumSize: Size(350,30),
+
+              ),
+            ),
+
+
+            Center(
+                child:Container(
+                  color : Colors.grey,
+                  width : 350,
+                  height : 1,
+                  margin: EdgeInsets.only(top:20,bottom: 0),
+
+                  ),
+
+            ),
+
+
+
+
+
+            SizedBox(height: 20,),
+
+            ElevatedButton(
+              onPressed: (){
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => LoginPage()
+              )
+              );
+
+                },
+              child: Text('로그아웃'),
+
+
+              //ElevatedButton 은 backgroundColor 속성이 없다.
+              //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    // shape : 버튼의 모양을 디자인 하는 기능
+
+                      borderRadius: BorderRadius.circular(10.0)
+                  ),
+                  minimumSize: Size(350,50),
+
+              ),
+            ),
+            SizedBox(height: 60,),
+            Center(
+              child: Text("Stockholic",style:TextStyle(fontSize: 15,color: Colors.black),),
+            ),
+
+
+          ]
+      ),
+    );
+  }
+}
+
+
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        resizeToAvoidBottomInset : false,
+      body : Column(
+    children: <Widget>[
+      SizedBox(height: 100,),
+      Center(
+        child:Container(
+          color : Colors.black,
+          width : 350,
+          height : 1,
+          margin: EdgeInsets.only(top:20,bottom: 20),
+
+        ),
+      ),
+
+Center(
+      child: Container(
+        padding: EdgeInsets.all(30.0),  //패딩을 줘서 좌우 간격을 띄운다.
+       child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                  labelText: 'id',
+                border: OutlineInputBorder(),
+              ),
+
+            ),
+            SizedBox(height: 10,),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            )
+          ],
+        ),
+      )
+),
+
+      ElevatedButton(
+        onPressed: (){
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => MyPage()
+              )
+          );
+
+        },
+        child: Text('로그인'),
+
+
+        //ElevatedButton 은 backgroundColor 속성이 없다.
+        //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
+        style: ElevatedButton.styleFrom(
+          primary: const Color(0x11BB1AC7),
+          shape: RoundedRectangleBorder(
+            // shape : 버튼의 모양을 디자인 하는 기능
+
+              borderRadius: BorderRadius.circular(10.0)
+          ),
+          minimumSize: Size(350,50),
+
+        ),
+      ),
+
+/*
+      Center(
+          child:Container(
+            color : Colors.green,
+            width : 350,
+            height : 50,
+            margin: EdgeInsets.only(top:0,bottom: 0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text('로그인',style: TextStyle(
+                  color: Colors.white
+              ),),
+            ),
+          )
+      ),
+
+*/
+      Center(
+        child:Container(
+          color : Colors.black,
+          width : 350,
+          height : 1,
+          margin: EdgeInsets.only(top:20,bottom: 0),
+        ),
+      ),
+      SizedBox(height: 240,),
+      Center(
+        child:Container(
+        child: Text("Stockholic",style:TextStyle(fontSize: 15,color: Colors.black),),
+       // margin: EdgeInsets.all(120),
+        ),
+      ),
+
+
+    ]
+      )
+    );
+  }
+}
+
+class info_correction extends StatelessWidget{
+  final _formKey = GlobalKey<FormState>();
+  final _nickname = TextEditingController();
+  final _email = TextEditingController();
+  UserModel user = Get.arguments;
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar : AppBar
+      body : Column(
+    children: <Widget>[
+    SizedBox(height: 100,),
+    Center(
+    child:Container(
+    color : Colors.black,
+    width : 350,
+    height : 1,
+    margin: EdgeInsets.only(top:20,bottom: 20),
+
+    ),
+    ),
+
+    Center(
+    child: Container(
+    padding: EdgeInsets.all(30.0),  //패딩을 줘서 좌우 간격을 띄운다.
+    child: Column(
+    children: <Widget>[
+    TextField(
+    decoration: InputDecoration(
+    labelText: 'id',
+    border: OutlineInputBorder(),
+    ),
+
+    ),
+    SizedBox(height: 10,),
+    TextField(
+    decoration: InputDecoration(
+    labelText: 'password',
+    border: OutlineInputBorder(),
+    ),
+
+    )
+    ],
+    ),
+    )
+    ),
+
+    ElevatedButton(
+    onPressed: (){
+    Navigator.push(context,
+    MaterialPageRoute(builder: (context) => MyPage()
+    )
+    );
+
+    },
+    child: Text('로그인'),
+
+
+    //ElevatedButton 은 backgroundColor 속성이 없다.
+    //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
+    style: ElevatedButton.styleFrom(
+    primary: const Color(0x11BB1AC7),
+    shape: RoundedRectangleBorder(
+    // shape : 버튼의 모양을 디자인 하는 기능
+
+    borderRadius: BorderRadius.circular(10.0)
+    ),
+    minimumSize: Size(350,50),
+
+    ),
+    ),
+
+/*
+      Center(
+          child:Container(
+            color : Colors.green,
+            width : 350,
+            height : 50,
+            margin: EdgeInsets.only(top:0,bottom: 0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text('로그인',style: TextStyle(
+                  color: Colors.white
+              ),),
+            ),
+          )
+      ),
+
+*/
+    Center(
+    child:Container(
+    color : Colors.black,
+    width : 350,
+    height : 1,
+    margin: EdgeInsets.only(top:20,bottom: 0),
+    ),
+    ),
+    SizedBox(height: 240,),
+    Center(
+    child:Container(
+    child: Text("Stockholic",style:TextStyle(fontSize: 15,color: Colors.black),),
+    // margin: EdgeInsets.all(120),
+    ),
+    ),
+
+
+    ]
+    )
+    );
+  }
+}
