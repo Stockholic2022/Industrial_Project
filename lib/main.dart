3a69170c048dc90aeb9339eccae0ed3640bce738
PARK <pdm7817@naver.com>
2022-12-09 04:04:13 +0900

    1209 0404

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,10 @@ class MyPage extends StatelessWidget {
 
             ElevatedButton(
               onPressed: (){
-                print('내 정보 수정');
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => re_info()
+                    )
+                );
               },
               child: Text('내 정보 수정'),
 
@@ -267,7 +270,7 @@ Center(
         //ElevatedButton 은 backgroundColor 속성이 없다.
         //ElevatedButton 에서는 primary 속성이 배경색을 담당한다.
         style: ElevatedButton.styleFrom(
-          primary: const Color(0x11BB1AC7),
+          primary: const Color(0x6CC633D0),
           shape: RoundedRectangleBorder(
             // shape : 버튼의 모양을 디자인 하는 기능
 
@@ -317,3 +320,117 @@ Center(
     );
   }
 }
+
+class re_info extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset : false,
+      body : Column(
+          children: <Widget>[
+
+            SizedBox(height: 60,),
+
+            Center(
+              child: Text("회원정보 수정",style:TextStyle(fontSize: 25,color: Colors.black),),
+            ),
+
+            SizedBox(height: 50,),
+
+            Center(
+                child: Container(
+                  padding: EdgeInsets.all(30.0),  //패딩을 줘서 좌우 간격을 띄운다.
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'E-MAIL',
+                          border: OutlineInputBorder(),
+                        ),
+
+                      ),
+                      SizedBox(height: 10,),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'password',
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 10,),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: '생년월일',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: '연락처',
+                          border: OutlineInputBorder(),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+            ),
+
+            Center(
+              child:Container(
+                color : Colors.grey,
+                width : 350,
+                height : 1,
+                margin: EdgeInsets.only(top:20,bottom: 0),
+
+              ),
+
+            ),
+
+            SizedBox(height: 20,),
+
+            ElevatedButton(
+              onPressed: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MyPage()
+                    )
+                );
+
+              },
+              child: Text('저장'),
+
+              style: ElevatedButton.styleFrom(
+                primary: const Color(0x57DA6532),
+                shape: RoundedRectangleBorder(
+                  // shape : 버튼의 모양을 디자인 하는 기능
+
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+                minimumSize: Size(350,50),
+
+              ),
+            ),
+
+            SizedBox(height: 20,),
+            Center(
+              child: Text("Stockholic",style:TextStyle(fontSize: 15,color: Colors.black),),
+            ),
+
+
+          ]
+      ),
+    );
+  }
+}
+
+
+class realtime_price extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+
+    );
+  }
+}
+
+
