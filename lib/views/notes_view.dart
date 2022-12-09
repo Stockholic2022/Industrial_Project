9afc2335d9911217a979c04cf782dfe217f0b936
PARK <pdm7817@naver.com>
2022-12-09 20:50:38 +0900

    notes_15

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -86,3 +86,7 @@ class _NotesViewState extends State<NotesView> {
 Future<bool> showLogOutDialog(BuildContext context) {
   return showDialog<bool>(
     context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign out'),
+        content: const Text('Are you sure you want to sign out?'),
