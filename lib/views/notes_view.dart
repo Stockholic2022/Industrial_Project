7b179732636253db69087802063f7af28fab61e8
PARK <pdm7817@naver.com>
2022-12-09 20:41:30 +0900

    notes_1

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:test1/constants/routes.dart';
+import 'package:test1/enums/menu_action.dart';
+import 'package:test1/services/auth/auth_service.dart';
+import 'package:test1/services/crud/notes_service.dart';
+
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  _NotesViewState createState() => _NotesViewState();
+}
