4c18e51d560db5d592bf42766b03777407d0f28b
sincheol <ohmong0803@gmail.com>
2022-12-09 23:57:27 +0900

    crud_exceptions1

--- lib/services/crud/crud_exceptions.dart
+++ lib/services/crud/crud_exceptions.dart
@@ -4,15 +4,3 @@ class DatabaseAlreadyOpenException implements Exception {}
 class UnableToGetDocumentsDirectory implements Exception {}
 
 class DatabaseIsNotOpen implements Exception {}
-
-class CouldNotDeleteUser implements Exception {}
-
-class UserAlreadyExitsts implements Exception {}
-
-class CouldNotFindUser implements Exception {}
-
-class CouldNotDeleteNote implements Exception {}
-
-class CouldNotFindNote implements Exception {}
-
-class CouldNotUpdateNote implements Exception {}
